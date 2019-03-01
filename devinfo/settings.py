498e98ebb0d65ca2202407561bd1c09c86abd5d3
shervin <shervin.ha@gmail.com>
2019-03-01 13:54:25 +0330

    ok

--- devinfo/settings.py
+++ devinfo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'iii3bc#u3%)tz*jypl^!@^x()@)ocgzlpqvmy3e$95*!*q&2zt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['devinfo.ir', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
