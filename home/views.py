28bd0a4acb1f779802badc378ca9eba337ca4b76
shervin <shervin.ha@gmail.com>
2019-03-03 01:00:35 +0330

    ok

--- home/views.py
+++ home/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 
+
+def price_print(url):
+    html_data = requests.get(url).text
+    ada_price = (html_data.split("ADAH19")[1].split("lastPrice"))[1][2:-2]
+    return ada_price
+
+ada_price = price_print('https://www.bitmex.com/app/trade/ADAH19')
+posts =[
+    {'date': 1900,
+    'title': ada_price}
+]
 def home(request):
-    return render(request, 'home/home.html')
+    ada = {
+        'data': posts
+    }
+    return render(request, 'home/home.html', ada)
